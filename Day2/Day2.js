677f384d210a18f0c0379f81672c653f2faf0378
Akshath-dev <akshathg.dev@gmail.com>
2024-01-05 21:00:19 +0530

    Create Day2.js

---
+++ Day2/Day2.js
@@ -0,0 +1,52 @@
+// Let is used for block level and var is for global level
+
+// let a = "95"
+
+// console.log(typeof a)
+
+// // converting string into integer
+// a = Number.parseInt()
+
+// console.log(typeof a)
+
+
+// for in loop 
+
+let obj = {
+    49:"akshath",
+    98:"gaana",
+    75:"sham",
+    80:"maruthi"
+}
+// for in loop
+for (let i in obj){
+    console.log("Marks of " + obj[i] +" is " + i)
+}
+
+
+// for of loop
+
+for (let i of "Akshath"){
+
+    console.log(i)
+}
+
+
+// functions in JS
+
+function adding(a, b){
+    return a+b;
+}
+
+const sum = (a, b)=>{
+    return a+b;
+}
+
+let a = 1;
+let b = 2;
+let c = 2;
+let p = adding(a,b);
+let q = sum(b,c);
+
+console.log('added number is ', p)
+console.log('added number is ', q)
